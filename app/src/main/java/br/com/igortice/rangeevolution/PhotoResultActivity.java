0822ca55d7166146f32dd9740035257e78afd0cb
Igor Rocha <igortice@gmail.com>
2016-04-02 20:33:33 -0300

    back to main photo

--- app/src/main/java/br/com/igortice/rangeevolution/PhotoResultActivity.java
+++ app/src/main/java/br/com/igortice/rangeevolution/PhotoResultActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,6 +19,14 @@ public class PhotoResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavUtils.navigateUpFromSameTask(PhotoResultActivity.this);
+            }
+        });
 
         Intent intent = getIntent();
         Bitmap bitmap = (Bitmap) intent.getParcelableExtra("BitmapImage");
