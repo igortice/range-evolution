8b4c3256b6d65675b001717ea996cfee5ef036c5
Igor Rocha <igortice@gmail.com>
2016-04-11 10:26:28 -0300

    layout new category

--- app/src/main/java/br/com/igortice/rangeevolution/PhotoResultActivity.java
+++ app/src/main/java/br/com/igortice/rangeevolution/PhotoResultActivity.java
@@ -1,16 +1,27 @@
 package br.com.igortice.rangeevolution;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PhotoResultActivity extends AppCompatActivity {
 
@@ -35,6 +46,30 @@ public class PhotoResultActivity extends AppCompatActivity {
         if (bitmap == null)
             bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.android);
         imageView.setImageBitmap(bitmap);
+
+        final Spinner dropdown = (Spinner)findViewById(R.id.spinner);
+        final ArrayList<String> items = new ArrayList<>(Arrays.asList("Escolha a categoria"));
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        Button button = (Button) findViewById(R.id.btnAddCat);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+                EditText editText = (EditText) findViewById(R.id.txtAddCat);
+                String val = editText.getText().toString();
+                editText.setText("");
+                if(!items.contains(val))
+                    items.add(val);
+                adapter.notifyDataSetChanged();
+                dropdown.setSelection(items.indexOf(val));
+            }
+        });
     }
 
 }
