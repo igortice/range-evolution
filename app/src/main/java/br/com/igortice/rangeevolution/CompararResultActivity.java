d133b927b78c89f1c79579c4711c30b9475923a6
Igor Rocha <igortice@gmail.com>
2016-05-23 10:42:13 -0300

    init show compare images

--- app/src/main/java/br/com/igortice/rangeevolution/CompararResultActivity.java
+++ app/src/main/java/br/com/igortice/rangeevolution/CompararResultActivity.java
@@ -5,32 +5,25 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CompararResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_comparar_result);
 
-        configToolBar();
-
         String[] mystringArray = getIntent().getStringArrayExtra("fotosSelecionadas");
-    }
-
-
-
-    private void configToolBar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NavUtils.navigateUpFromSameTask(CompararResultActivity.this);
-            }
-        });
+        TextView txtData1 = (TextView) findViewById(R.id.txtData1);
+        txtData1.setText(mystringArray[0]);
+        TextView txtData2 = (TextView) findViewById(R.id.txtData2);
+        txtData2.setText(mystringArray[1]);
     }
 
 }
